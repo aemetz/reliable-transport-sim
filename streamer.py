033715c9fccca6e658bedb1f586de4d5685ed16a
aemetz <aemetz38@gmail.com>
2023-10-22 20:55:27 -0500

    part 3 stage b pass

--- streamer.py
+++ streamer.py
@@ -4,6 +4,7 @@ from lossy_socket import LossyUDP
 from socket import INADDR_ANY
 import sys
 from struct import unpack, pack
+import time
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -21,12 +22,11 @@ class Streamer:
         self.seq = 0
         self.rec_seq = 0
         self.buffer = {}
-        self.mtu = 1470
+        self.mtu = 1469
         self.closed = False
+        self.ack = False
         executor = ThreadPoolExecutor(max_workers = 1)
         executor.submit(self.listener)
-
-        #
 
 
     def listener(self):
@@ -39,87 +39,66 @@ class Streamer:
                 if data == b'':
                     continue
                 
-                unpacked = unpack('@H1470s', data)
+                unpacked = unpack('@Hc1469s', data)
                 unpacked_seq_num = unpacked[0]
-                unpacked_data = unpacked[1].split(b'\x00')[0]
+                is_ack = unpacked[1]
+
+                unpacked_data = unpacked[2].split(b'\x00')[0]
 
                 # stores data in buffer
-                self.buffer[unpacked_seq_num] = unpacked_data
+                if is_ack == b'0':
+                    self.buffer[unpacked_seq_num] = unpacked_data
+                else:
+                    self.ack = True
                 # self.recv()
                 
             except Exception as e:
-                print("listener died!")
-                print(e)
+                if not self.closed:
+                    print("listener died!")
+                    print(e)
 
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         len = sys.getsizeof(data_bytes)
-        # print(len)
-        # print(data_bytes)
         
-        
-        # j = 0
         if len > self.mtu:
             for i in range(0, len, self.mtu):
                 if i+self.mtu > len:
-                    data = pack('@H1470s', self.seq, data_bytes[i:len-1])
+                    # 0 for data, 1 for ack
+                    data = pack('@Hc1469s', self.seq, b'0', data_bytes[i:len-1])
                     self.socket.sendto(data, (self.dst_ip, self.dst_port))
                 else:
-                    data = pack('@H1470s', self.seq, data_bytes[i:i+self.mtu])
+                    data = pack('@Hc1469s', self.seq, b'0', data_bytes[i:i+self.mtu])
                     print(type(data))
                     
                     self.socket.sendto(data, (self.dst_ip, self.dst_port))
                 self.seq += 1
         else:
-        # for now I'm just sending the raw application-level data in one UDP payload
-            data = pack('@H1470s', self.seq, data_bytes)
-            # print(type(data))
+            data = pack('@Hc1469s', self.seq, b'0', data_bytes)
             self.socket.sendto(data, (self.dst_ip, self.dst_port))
             self.seq += 1
 
+        while not self.ack: time.sleep(0.01)
+
+        self.ack = False
+
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        
 
+        total_data = b''
 
-        # data, addr = self.socket.recvfrom()
-        # size = sys.getsizeof(data)
-        # unpacked = unpack('@H1470s', data)
+        while self.buffer.get(self.rec_seq):
+            total_data += self.buffer[self.rec_seq]
 
+            ack = pack('@Hc1469s', self.rec_seq, b'1', b'')
+            self.socket.sendto(ack, (self.dst_ip, self.dst_port))
 
-        # unpacked_seq_num = unpacked[0]
-        # unpacked_data = unpacked[1].split(b'\x00')[0]
-        # size = sys.getsizeof(unpacked_data)
-        
-
-        # if we receive an unpacked_seq_num out of order (!= rec_seq)
-        # we store it in buffer as key w value data
-        # when 
-        # print(unpacked_data)
-        # print(type(unpacked_seq_num))
-        # print(type(self.rec_seq))
-
-        if self.buffer.get(self.rec_seq):
-            total_data = self.buffer[self.rec_seq]
-
-            while self.buffer.get(self.rec_seq + 1) != None:
-               total_data += self.buffer[self.rec_seq + 1]
-               self.rec_seq += 1
-            
             self.rec_seq += 1
-            print(total_data)
-            return total_data
+            
+        return total_data
         
-        # print(f"TYPE: {type(data)}")
-        # print(f"DATA: {data}")
-        # print(f"Unpacked DATA: {unpacked_data}")
         
-        # For now, I'll just pass the full UDP payload to the app
-        return b''
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
